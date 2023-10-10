6858120622f1c000c5129596df9ef1b44fc11e82
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2023-10-10 07:38:07 +0530

    intial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 int main (){
+    int a =8;
+    cout<< a <<endl;
     cout<<"######## TAPABRATA GOSWAMI #########";
     cout<<"HEllo World";
     return 0;
